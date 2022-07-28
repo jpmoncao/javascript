1c62e61bdd57fbd4339407c7378b2fff6ddf3386
João Pedro Monção <pedrohjoao44@gmail.com>
2022-07-28 17:40:31 -0300

    📒 Aula de recursos oferecidos de forma nativo pelos navegadores, usando o console.

---
+++ introducao/aula003.js
@@ -0,0 +1,10 @@
+// Recursor nativos do browser
+
+console.log("Olá, mundo!") // Retorna mensagem neutra para o console
+console.info("Isso é uma informação") // Retorna mensagem de informação para o console
+console.warn("Isso é um aviso!") // Retorna mensagem de aviso para o console
+console.error("Ocorreu um erro...") // Retorna mensagem de erro para o console
+
+alert("Olá, mundo!") // Exibe um popup de alerta na tela
+confirm("JavaScript é legal! Confirma essa informação?") // Exibe um popup de caixa de confirmação
+prompt("Qual seu nome?") // Exibe um popup de input de dados
