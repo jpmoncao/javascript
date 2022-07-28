c4e796dae53e9e74e82e49dd91dfdb4b4ef51182
João Pedro Monção <pedrohjoao44@gmail.com>
2022-07-28 17:52:13 -0300

    📒 Aula sobre variáveis -- tipo var, let e const -- seus usos e situações. (part. 1)

---
+++ introducao/aula004.js
@@ -0,0 +1,27 @@
+// Variavéis
+//      As variavéis são recurso que utilizamos para armazenarmos valores, e usarmos
+//      futuramente no nosso código. Temos 3 tipos de variável.
+
+// Let (valores variáveis):
+let spaceship = "FE Supernova"  // Cria variavel e atribui o valor "FE Supernova"
+console.log(spaceship)          // = "FE Supernova" (retorna o valor da variável)
+spaceship = "FE Helmet"         // Troco o valor da variável
+console.log(spaceship)          // = "FE Helmet" (retorna o novo valor da variável)
+
+// Const (valores constantes):
+const squad = "Estelar"  // Cria constante e atribui o valor "Estelar"
+console.log(squad)       // = "Estelar" (retorna o valor da constante)
+squad = "Espacial"       // Tenta trocar o valor **NÃO É POSSÍVEL** (valor é constante)
+console.log(squad)       // = "Estelar" (retorna o valor da constante)
+
+// Var (valores variáveis, herança de outras linguagens)
+var velocity = 80       // Cria variavel e atribui o valor "FE Supernova"
+console.log(velocity)   // = 80 (retorna o valor da variável)
+velocity = 25           // Troco o valor da variável
+console.log(velocity)   // = 25 (retorna o novo valor da variável)
+
+// Variáveis sem valores
+let noValue
+console.log(noValue) // = undefined
+// EXPLICAÇÃO: Isso acontece porque a variável foi apenas criada, mas não
+//             tem valor atribuido, assim, ficando undefined.
