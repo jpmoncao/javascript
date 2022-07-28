9ba2e386f95eb249ede329a662bfaa23775af139
João Pedro Monção <pedrohjoao44@gmail.com>
2022-07-28 17:30:12 -0300

    📒 Aula introdutória sobre tipos primitivos no JavaScript

---
+++ introducao/aula001.js
@@ -0,0 +1,8 @@
+// Tipos Primitivos
+//     Os tipos primitivos são a base de um código, utilizamos para representar um dado "bruto".
+//     Com estes tipos que somos capazes de construir  estrutura mais complexas.
+
+//     Number: 1, 0.3333, 4, -90, 04, 0
+//     String: "Olá mundo", "Oi", "JavaScript"
+//     Boolean: true, false, null, undefined
+
