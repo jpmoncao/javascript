945fe8031795afedd7128185a6385c4bc713b577
João Pedro Monção <pedrohjoao44@gmail.com>
2022-07-28 17:30:41 -0300

    📒 Aula sobre operações básicas usando os tipos primitivos do JS

---
+++ introducao/aula002.js
@@ -0,0 +1,18 @@
+// Operações básicas
+
+// Numbers:
+    1 + 1 // = 2
+    1 - 2 // = -1
+    4 / 2 // = 2
+    2 * 2 // = 4
+    10 % 3 // = 1
+    1 + 5 * 5 // = 26
+
+// String:
+    `PlanetaTerra` /* = "PlanetaTerra"*/ 
+    "Planeta " + "Terra" // = "Planeta Terra"
+    "Planeta"[2] // = "a"
+    "Planeta Terra".length // = 13 (contando o espaço)
+    "Planeta " + "Terra"[2] // = "Planeta r"
+    ("Planeta " + "Terra")[2] // = "a"
+    "Eu vivo na Terra a " + 30 + "anos" // = "Eu vivo na Terra a 30 anos"
