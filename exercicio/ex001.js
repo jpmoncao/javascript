ad70d5c2c6c2a1e8678f2de93a6068dfec380b16
João Pedro Monção <pedrohjoao44@gmail.com>
2022-07-29 16:14:26 -0300

    ✅ Exercício 001: Usando recurso do browser manipule respostas do user

---
+++ exercicio/ex001.js
@@ -0,0 +1,7 @@
+alert("Bem-vindo! A seguir pediremos que informe alguns dados.")
+
+let nome = prompt("Qual seu nome?")
+let age = prompt("Qual sua idade?")
+let ageConfirmation = confirm(`Sua idade é ${age} anos. Confirma essa informação?`)
+
+alert(`Nome: ${nome}\nIdade:${age}\nIdade confirmada: ${ageConfirmation}`)
