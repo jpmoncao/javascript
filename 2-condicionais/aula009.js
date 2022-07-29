8aef0c09b15ce5f7321bcefa39dfadfa6c35d634
João Pedro Monção <pedrohjoao44@gmail.com>
2022-07-29 17:35:21 -0300

    📒 Aula sobre operações lógicas (teoria)

---
+++ 2-condicionais/aula009.js
@@ -0,0 +1,8 @@
+// Operadores lógicos part. 1
+
+/*
+    São expressões construídas a partir de operações com valores booleanos, retornando
+    um valor booleano.
+    Existem ainda as expressões lógicas que são conjuntos de condições lógicas usando
+    os operados lógicos -- E(and), OU(or) e NÃO(not).
+*/
