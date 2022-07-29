64393525b3cc8551e7d97e099afcdbf28bafcf3a
João Pedro Monção <pedrohjoao44@gmail.com>
2022-07-29 16:21:46 -0300

    📒 Aula sobre comentários em código

---
+++ introducao/aula006.js
@@ -0,0 +1,14 @@
+// Comentários
+
+/* Isso é um comentário, ele é ignorado pelo JS, e serve para...
+   COMENTAR ALGO ºoº */
+
+let number = 1
+//number = 4 (é ignorado)
+console.log(number) // = 1
+
+//Comentários longos
+/*      Para comentários longos
+        usamos os simbolos de 
+        barra asterisco.
+*/
