9a357c3a532de41cc4eb599bb6aa2628233ade38
João Pedro Monção <87674021+jpmoncao@users.noreply.github.com>
2022-09-21 15:58:19 -0300

    📒 Aula sobre padronização de Objetos (fix)

--- 6-objetos/aula030.js
+++ 6-objetos/aula030.js
@@ -1,7 +1,14 @@
 // Objetos (parte 2)
 
 // Exemplo 1:
-
+let spacialStation = {
+    name: "Fox",
+    platformsQuantity: 10,
+    "new name": "Estelar",
+    true: false,
+    2: "Descoberta"
+    // É possivel declarar chaves de todas maneiras, mas podemos esbarrar em exceções...
+}
 
 // Exceção (INTEIROS):
 console.log(spacialStation.2)    // Exibe: Uncaught SyntaxError: missing ) after argument list
@@ -10,4 +17,4 @@ console.log(spacialStation["2"]) // Exibe: Descoberta
 
 // Exceção (ALFANUMÉRICOS)
 console.log(spacialStation.new name)     // Exibe: Uncaught SyntaxError: missing ) after argument list
-console.log(spacialStation["new name"])  // Exibe: Estelar
+console.log(spacialStation["new name"])  // Exibe: "Estelar"
