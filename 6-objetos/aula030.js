56770e534d26ea36870825f71a06235f4bb30449
João Pedro Monção <87674021+jpmoncao@users.noreply.github.com>
2022-09-21 15:57:42 -0300

    📒 Aula sobre padronização de Objetos

---
+++ 6-objetos/aula030.js
@@ -0,0 +1,13 @@
+// Objetos (parte 2)
+
+// Exemplo 1:
+
+
+// Exceção (INTEIROS):
+console.log(spacialStation.2)    // Exibe: Uncaught SyntaxError: missing ) after argument list
+console.log(spacialStation[2])   // Exibe: Descoberta
+console.log(spacialStation["2"]) // Exibe: Descoberta
+
+// Exceção (ALFANUMÉRICOS)
+console.log(spacialStation.new name)     // Exibe: Uncaught SyntaxError: missing ) after argument list
+console.log(spacialStation["new name"])  // Exibe: Estelar
