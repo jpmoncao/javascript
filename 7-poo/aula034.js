4ddbc4981734744f86fc24bc941640d8fc13eaaa
João Pedro Monção <87674021+jpmoncao@users.noreply.github.com>
2022-09-22 16:18:04 -0300

    📒 Aula introdutória sobre programação orientada a objetos

---
+++ 7-poo/aula034.js
@@ -0,0 +1,17 @@
+// Programação orientada a objetos
+/*
+    • Toda linguagem possui um paradigma
+        ▪ Elas permitem a estruturação do código
+    • POO vem de Programação Orientada a Objetos
+    • Tem a ideia de abstrair, dentro do código, objetos da vida real
+    • Sua base são Classes e Objetos
+*/
+
+// Exemplo 1:
+class Spaceship { // Classe
+}
+
+let mySpaceship = new Spaceship()
+
+console.log(mySpaceship) // Exibe: Object {  }
+
