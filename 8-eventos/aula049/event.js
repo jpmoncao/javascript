df82f4da148b4abbf291320047d781ef693dbe0b
João Pedro Monção <87674021+jpmoncao@users.noreply.github.com>
2022-09-26 22:56:00 -0300

    🔄 Em processo: Aula sobre eventos

---
+++ 8-eventos/aula049/event.js
@@ -0,0 +1,17 @@
+// Eventos
+/*
+    • Eventos são ações que ocorrem no sistema:
+        ▪ Podem ocorrer por uma ação do usuário: como um clique
+        de um mouse ou mudança do valor de um campo.
+        ▪ Ou simplesmente como um fluxo do próprio sistema: como
+        a página concluir seu carregamento.
+    • No HTML conseguimos mapear os eventos e definir ações;
+*/
+
+function clickMe() {
+    console.log("Fui clicado.")
+}
+
+function focusMe() {
+    console.log("Fui focado")
+}
