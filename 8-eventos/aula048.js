c4963e11ddf07f9f64562647c7ada21a97b0b744
João Pedro Monção <87674021+jpmoncao@users.noreply.github.com>
2022-09-26 22:16:55 -0300

    📒 Aula sobre os módulos Date e Math

---
+++ 8-eventos/aula048.js
@@ -0,0 +1,13 @@
+console.log("Módulos Math e Date")
+
+// Módulo Math
+console.log(Math.pow(5, 3)) // Exibe: 125
+console.log(Math.sqrt(64))  // Exibe: 8
+console.log(Math.PI)        // Exibe: 3.141592653589793
+
+// Módulo Date
+let myBirthday = new Date(2022, 06, 10)
+console.log(myBirthday) // Exibe: Sun Jul 10 2022 00:00:00 GMT-0300 (Horário Padrão de Brasília)
+
+let today = Date.now()
+console.log(today) // Exibe: 1664241287921
