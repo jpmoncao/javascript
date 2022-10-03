088cd99636feaeca8d2d60a5357407e3e7e70358
João Pedro Monção <87674021+jpmoncao@users.noreply.github.com>
2022-10-03 15:15:58 -0300

    📒 Aula sobre eventos a partir do JavaScript

--- 8-eventos/aula049/event.js
+++ 8-eventos/aula049/event.js
@@ -9,9 +9,21 @@
 */
 
 function clickMe() {
-    console.log("Fui clicado.")
+    console.log("Fui clicado!")
 }
 
 function focusMe() {
-    console.log("Fui focado")
+    console.log("Fui focado!")
+}
+
+function blurMe() {
+    console.log("Fui desfocado!")
+}
+
+function changeMe() {
+    console.log("Fui alterado!")
+}
+
+function hoverMe() {
+    console.log("Mouse aqui")
 }
