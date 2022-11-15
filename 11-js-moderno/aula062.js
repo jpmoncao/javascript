40e2b5902b7d1587208b5cfdf2d83860be8015ab
João Pedro Monção <pedrohjoao44@gmail.com>
2022-11-15 00:10:40 -0300

    📒 Aula sobre interpolação de Strings

---
+++ 11-js-moderno/aula062.js
@@ -0,0 +1,13 @@
+// Interpolação de String
+
+// Exemplo 01
+let spaceshipName = 'Colossus'
+console.log(`Nome da nave: ${spaceshipName}`) // Exibe: Nome da nave: Colossus
+
+// Exemplo 02
+let newName = `Nome da nave: ${spaceshipName}`
+console.log(newName) // Exibe: Nome da nave: Colossus
+
+// Exemplo 03
+let velocity = 50
+console.log(`Velocidade da nave: ${velocity}km/s`) // Exibe: Velocidade da nave: 50km/s
