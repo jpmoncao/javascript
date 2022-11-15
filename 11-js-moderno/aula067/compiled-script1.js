dcfbd5f904604dd2287db7100c6c6d6c24a36144
João Pedro Monção <pedrohjoao44@gmail.com>
2022-11-15 02:08:53 -0300

    📒 Aula sobre transpilação de arquivos com Babel

---
+++ 11-js-moderno/aula067/compiled-script1.js
@@ -0,0 +1,3 @@
+function testingES8(text1, text2) {
+  alert(`${text1}\n${text2}`);
+}
---
+++ 11-js-moderno/aula067/compiled-script2.js
@@ -0,0 +1,5 @@
+"use strict";
+
+function testingES8(text1, text2) {
+  alert("".concat(text1, "\n").concat(text2));
+}
